e4180ba13245a10c073963ab71945dae60bae32d
dantemessy <thepunishar1@gmail.com>
2020-02-06 01:52:33 +0200

    schema fixed + error handler added

--- models/categories/categories-schema.js
+++ models/categories/categories-schema.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 
 const categories = mongoose.Schema({
   name: {type : String , require : true},
-  kills : {type : Number , require : true},
-  titles : {type : Array , require : true },
+  kills : {type : Number },
+  titles : {type : Array },
 });
 
 module.exports = mongoose.model('categories', categories);
